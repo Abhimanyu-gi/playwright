7b3ebf0840956dd761624eab096fc3a6fbc16a58
Abhimanyu-gi <160107552+Abhimanyu-gi@users.noreply.github.com>
2024-08-17 15:54:17 +0530

    Add files via upload

---
+++ project.spec.js
@@ -0,0 +1,55 @@
+const{ test, expect } = require('@playwright/test')
+test('Verify Logo', async ({ page }) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page.getByRole('img', { name: 'company-branding' }).click();
+    await expect(page).toBeTruthy();
+  });
+test("Valid login page", async function({page}){
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    await page.getByPlaceholder("Username").fill('Admin')
+    await page.getByPlaceholder("Password").fill('admin123')
+    await page.locator("//button[@type='submit']").click()
+    await expect(page).toHaveURL(/dashboard/);
+})
+test('Logout', async ({ page }) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page.getByPlaceholder('Username').fill('Admin');
+    await page.getByPlaceholder('Password').click();
+    await page.getByPlaceholder('Password').fill('admin123');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await page.getByRole('banner').getByRole('img', { name: 'profile picture' }).click();
+    await page.getByRole('menuitem', { name: 'Logout' }).click();
+    await expect(page).toHaveURL(/login/);
+  });
+/*
+  test('Forgot Password', async ({ page }) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page.getByText('Forgot your password?').click();
+    await page.getByPlaceholder('Username').click();
+    await page.getByPlaceholder('Username').fill('Admin');
+    await page.getByRole('button', { name: 'Reset Password' }).click();
+    await page.getByText('Reset Password link sent successfullyA reset password link has been sent to you').click();
+    await expect(page).toHaveURL("sendPasswordReset");
+  });
+  */
+/*
+  test('Invalid Credentials', async ({ page }) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page.getByPlaceholder('Username').fill('Admin');
+    await page.getByPlaceholder('Password').click();
+    await page.getByPlaceholder('Password').fill('ADMIN123');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await page.getByRole('alert').locator('div').filter({ hasText: 'Invalid credentials' }).click();
+    await expect(page).getByText("Invalid Credentials");
+  });
+  */
+  test('Support page', async ({ page }) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page.getByPlaceholder('Username').click();
+    await page.getByPlaceholder('Username').fill('Admin');
+    await page.getByPlaceholder('Password').click();
+    await page.getByPlaceholder('Password').fill('admin123');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await page.getByRole('banner').getByRole('img', { name: 'profile picture' }).click();
+    await expect(page).toBeTruthy();
+  });
